27d32b7ea1ee8af181e8371ea4474a82474b2119
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:18:06 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "virtual_hub_route_table_routes" {
+  description = "All virtual_hub_route_table_route resources"
+  value       = azurerm_virtual_hub_route_table_route.virtual_hub_route_table_routes
+}
+
